55b1015a4a2d933095f5b1ab0b3577a469e19023
jamesrohan <james.reckon@gmail.com>
2017-09-30 15:02:54 -0400

    First Commit
    First Commit

---
+++ hangman.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+ <head>
+  <title>Hangman</title>
+  <link rel="stylesheet" type="text/css" href=""> </link>	
+  <style>
+	<?php include ''; ?>
+  </style>
+
+ </head>
+ <body style="text-align: center; content-align: center;">
+ 
+	<p>
+		<h1> Hangman </h1>
+	</p>
+	
+ 
+ </body>
+</html>
