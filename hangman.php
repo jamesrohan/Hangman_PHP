d0116d0b56a399e7d21e0e4a10f13074766d9fc5
jamesrohan <james.reckon@gmail.com>
2017-10-01 13:32:34 -0400

    Third Commit
    Iterates through images for the wrong user input

--- hangman.php
+++ hangman.php
@@ -63,6 +63,7 @@ session_start();
 					if(!isset($_SESSION["CLUE_IDX"])){ // We dont want this variable to be reset everytime the page is refreshed, so we set it only 
 														// when it is not set. i.e the first time
 						$_SESSION["CLUE_IDX"] = mt_rand(0,1); // The Zero to One correspond to the size of the $words array(size = 2)
+						$_SESSION["IMG_IDX"] = 0 ;
 					}
 					
 					//Making $_SESSION["CLUE_IDX"] more redable
@@ -94,6 +95,7 @@ session_start();
 					
 				
 					//Image Index to go through the Array of Images
+					//Depricated
 					$img_idx = 0;
 				
 					//Seeing if the User hit the Submit button. On the screen the button will show as "Enter". And making sure the User Input is not empty
@@ -104,8 +106,10 @@ session_start();
 						
 						
 							//Checking to see if the User Input Ltter is inside of the word. 
-							if( stripos($words[$_SESSION["CLUE_IDX"]], $user_input) !== false ){
-								echo "<img src=\" ". $images[0] .  " \" >"."</img>";
+							if( stripos($words[$_SESSION["CLUE_IDX"]], $user_input) !== true ){
+								echo "<img src=\" ". $images[$_SESSION["IMG_IDX"]] .  " \" >"."</img>";
+								$_SESSION["IMG_IDX"]++;
+								
 							}
 						
 					}
